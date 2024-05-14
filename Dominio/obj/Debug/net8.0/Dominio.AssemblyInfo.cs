21a7c35edc92dfbcd0abcb0cecb4b57e05ebad60
Richard Vepo <richardvepogg@gmail.com>
2024-05-13 22:59:50 -0300

    novo VisualStudio gitignote
    criei gitignore para as coisas do visual studio

--- Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
+++ Dominio/obj/Debug/net8.0/Dominio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ea010cecf74f6ad0ec864b191935d72e280722c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b257fe9db9c2f9bb82624c5ad0fbd574cc9bda")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dominio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
