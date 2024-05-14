c37997b027034b543a6b12b8ccae6ac485dee153
Richard Vepo <richardvepogg@gmail.com>
2024-05-13 23:02:50 -0300

    csharp.gitignore

--- AcessoDados/obj/Debug/net8.0/AcessoDados.AssemblyInfo.cs
+++ AcessoDados/obj/Debug/net8.0/AcessoDados.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcessoDados")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b257fe9db9c2f9bb82624c5ad0fbd574cc9bda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a7c35edc92dfbcd0abcb0cecb4b57e05ebad60")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcessoDados")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcessoDados")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CadastroProduto/obj/Debug/net8.0/CadastroProduto.AssemblyInfo.cs
+++ CadastroProduto/obj/Debug/net8.0/CadastroProduto.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CadastroProduto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89b257fe9db9c2f9bb82624c5ad0fbd574cc9bda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a7c35edc92dfbcd0abcb0cecb4b57e05ebad60")]
 [assembly: System.Reflection.AssemblyProductAttribute("CadastroProduto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CadastroProduto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
