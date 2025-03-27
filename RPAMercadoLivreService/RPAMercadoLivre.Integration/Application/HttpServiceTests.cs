5509a11ebfc83c648c63bfa56c2d7704f56a8df4
Richard Vepo <richardvepogg@gmail.com>
2025-03-27 20:23:17 -0300

    teste de integração
    criado teste de integração no projeto RPAMercadoLivre que consulta o site com serviço HTTPService e verifica se veio o esperado

---
+++ RPAMercadoLivreService/RPAMercadoLivre.Integration/Application/HttpServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using RPAMercadoLivre.Integration.Helpers;
+using RPAMercadoLivreProjeto.Application.Services;
+
+namespace RPAMercadoLivre.Unit.Application
+{
+    public class HttpServiceTests
+    {
+
+        [Theory(DisplayName = "Query the product on the Mercado Libre website and verify if it was able to find the product information.")]
+        [InlineData("kindle")] 
+        [InlineData("iphone15")] 
+        public async Task GetHttpResponseBodyAsync_DeveRetornarConteudoDoMercadoLivre_ComInformacoesEsperadas(string produto)
+        {
+            var httpService = new HttpService();
+
+            // Act
+            var responseBody = await httpService.GetHttpResponseBodyAsync($"{MercadoLivreHelper.Url}{produto}");
+
+            // Assert
+            responseBody.Should().NotBeNullOrEmpty();
+
+            var match = MercadoLivreHelper.PrecosRegex.Match(responseBody);
+
+            match.Success.Should().BeTrue();
+        }
+    
+
+    }
+
+}
---
+++ RPAMercadoLivreService/RPAMercadoLivre.Integration/Helpers/MercadoLivreHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RPAMercadoLivre.Integration.Helpers
+{
+    public static class MercadoLivreHelper
+    {
+        public static readonly Regex PrecosRegex = new Regex(
+            pattern: @"R\$\s*\s*<\s*/\s*span\s*>\s*<\s*span\s*class\s*=\s*(?:""|')andes-money-amount__fraction\s*(?:""|')[^>]*>(?<valorAVista>[^<]*)<\s*/\s*span\s*>\s*<\s*/\s*span\s*>\s*<\s*span\s*class",
+            options: RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline,
+            matchTimeout: TimeSpan.FromSeconds(15)
+        );
+
+        public static string Url = "https://lista.mercadolivre.com.br/";
+    }
+}
